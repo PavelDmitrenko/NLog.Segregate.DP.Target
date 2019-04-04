d54e9de581f4c408c4bcf19a23031d36b5059f8f
dmitrenko <dmitrenko@fas.gov.ru>
2019-04-04 15:13:10 +0300

    Now SegregateTarget is derived from FilteTarget

--- NLog.Syslog.DP.Target/NLog.Segregate.DP.Target.cs
+++ NLog.Syslog.DP.Target/NLog.Segregate.DP.Target.cs
@@ -1,61 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using NLog.Internal;
+using NLog.Layouts;
 using NLog.Targets;
 
 namespace NLog.Segregate.DP.Target
 {
 
 	[Target("Segregate")]
-	public class SegregateTarget : TargetWithLayout
+	public class SegregateTarget : FileTarget
 	{
 
-		public string logsDirectory { get; set; }
+		public Layout segregateFileName { get; set; }
 		public int filesToArchive { get; set; }
-		private string _directory { get; set; }
-		private readonly object threadLock = new object();
+		private readonly object _threadLock = new object();
 
+		public Layout segregateArchiveFileName{ get; set; }
 
 		public SegregateTarget()
 		{
+			CreateDirs = true;
+			FileName = "";
 		}
 
 		protected override void InitializeTarget()
 		{
+			FileName = segregateFileName;
 			base.InitializeTarget();
 		}
 
 		protected override void Write(LogEventInfo logEvent)
 		{
-			string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-			_directory = Path.Combine(baseDirectory, logsDirectory);
-
-			if (!Directory.Exists(_directory))
-				Directory.CreateDirectory(_directory);
-
-			string logMessage = this.Layout.Render(logEvent);
-			string fileName = $"{DateTime.Now:yyyyMMdd_HHmmss_fffffff}.log";
-			File.WriteAllText($"{_directory}{fileName}", logMessage);
-
+			base.Write(logEvent);
 			_Archive();
-
 		}
 
 		private void _Archive()
 		{
-			lock (threadLock)
+			lock (_threadLock)
 			{
 
-				DirectoryInfo DirInfo = new DirectoryInfo(_directory);
+				string archiveName = CleanupInvalidFilePath(segregateArchiveFileName.Render(new LogEventInfo()));
+				string logDirectory = Path.GetDirectoryName(Path.GetFullPath(FileName.Render(new LogEventInfo())));
+				string logExtension = Path.GetExtension(FileName.Render(new LogEventInfo()));
 
-				var files = DirInfo.EnumerateFiles("*.log")
-					.Where(x => x.CreationTime < DateTime.Now.AddSeconds(-5))
+				DirectoryInfo DirInfo = new DirectoryInfo(logDirectory);
+
+				List<FileInfo> files = DirInfo.EnumerateFiles($"*{logExtension}")
+					.Where(x => x.CreationTime < DateTime.Now.AddMilliseconds(100))
 						.OrderBy(x => x.CreationTime).Take(filesToArchive).ToList();
 
 				if (files.Count == filesToArchive)
 				{
-					string zipName = Path.Combine(_directory, $"{DateTime.Now:yyyyMMdd_HHmmss_fffffff}.zip");
+					string zipName = Path.GetFullPath(archiveName);
 
 					using (ZipArchive newFile = ZipFile.Open(zipName, ZipArchiveMode.Create))
 					{
@@ -63,7 +63,6 @@ namespace NLog.Segregate.DP.Target
 						{
 							newFile.CreateEntryFromFile(file.FullName, file.Name, CompressionLevel.Fastest);
 						}
-
 					}
 
 					foreach (FileInfo file in files)
@@ -88,5 +87,32 @@ namespace NLog.Segregate.DP.Target
 			base.CloseTarget();
 		}
 
+		// From FilePathLayout.cs
+		private static readonly HashSet<char> InvalidFileNameChars = new HashSet<char>((IEnumerable<char>)Path.GetInvalidFileNameChars());
+		private static string CleanupInvalidFilePath(string filePath)
+		{
+			if (string.IsNullOrWhiteSpace(filePath))
+				return filePath;
+
+			int num = filePath.LastIndexOfAny(new[] {Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar});
+
+			char[] chArray = (char[])null;
+			for (int index = num + 1; index < filePath.Length; ++index)
+			{
+				if (InvalidFileNameChars.Contains(filePath[index]))
+				{
+					if (chArray == null)
+						chArray = filePath.Substring(num + 1).ToCharArray();
+
+					chArray[index - (num + 1)] = '_';
+				}
+			}
+
+			if (chArray != null)
+				return Path.Combine(num > 0 ? filePath.Substring(0, num + 1) : string.Empty, new string(chArray));
+
+			return filePath;
+		}
 	}
+
 }
--- Tests/Tests/Program.cs
+++ Tests/Tests/Program.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog;
+using NLog.Fluent;
 using NLog.Extensions.Logging;
 using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
@@ -15,44 +16,17 @@ namespace Tests
 		
 		static void Main(string[] args)
 		{  
-			
-			var servicesProvider = BuildDi();
-			var runner = servicesProvider.GetRequiredService<Runner>();
+			GlobalDiagnosticsContext.Set("DeviceID", "SomeDeviceID");
+			Logger logger = LogManager.GetCurrentClassLogger();
 
-			runner.DoAction("Action1");
+			while (true)
+			{
+				logger.Info(DateTime.Now);
+				System.Threading.Thread.Sleep(500);
+			}
+
+
 			Console.ReadLine();
-		}
-		private static ServiceProvider BuildDi()
-		{
-			return new ServiceCollection()
-				.AddLogging(builder => {
-					builder.SetMinimumLevel(LogLevel.Trace);
-					builder.AddNLog(new NLogProviderOptions {
-						CaptureMessageTemplates = true,
-						CaptureMessageProperties = true
-					});
-				})
-				.AddTransient<Runner>()
-				.BuildServiceProvider();
-		}
-
-		public class Runner
-		{
-			private readonly ILogger<Runner> _logger;
-
-			public Runner(ILogger<Runner> logger)
-			{
-				_logger = logger;
-			}
-
-			public void DoAction(string name)
-			{
-				Logger logger = NLog.LogManager.GetCurrentClassLogger();
-				logger.Info("zzz");
-				
-			}
-
-
 		}
 
 	}
